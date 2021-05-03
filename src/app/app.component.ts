612e649b95883162821879b97f0bd3e726a12b8d
ChrisHursty <chris@iamchrishurst.com>
2021-05-03 19:13:36 -0400

    Added products and loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root',
   template: `
-  <div>
-    <pm-products></pm-products>
-  </div>
-  `
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+        <a class='navbar-brand'>{{pageTitle}}</a>
+        <ul class='nav nav-pills'>
+          <li><a class='nav-link' routerLinkActive='active' routerLink='/welcome'>Home</a></li>
+          <li><a class='nav-link' routerLinkActive='active' routerLink='/products'>Product List</a></li>
+        </ul>
+    </nav>
+    <div>
+      <pm-products></pm-products>
+    </div>
+    `
   
 })
 export class AppComponent {
